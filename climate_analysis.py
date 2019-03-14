16917065be01bbbf15922ba0c7db14f28eb719b7
Aled Evans <A.Evans@soton.ac.uk>
2019-03-14 14:01:26 +0000

    added a rainfall processing placeholder

--- climate_analysis.py
+++ climate_analysis.py
@@ -26,3 +26,6 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+#TODO(Aled): Add a call to process rainfall
+
+
